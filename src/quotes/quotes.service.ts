ee8f21fdf3218c3cbaf67da26a39e3486e29e022
JacekCzupyt <kuba.czupyt@gmail.com>
2021-05-21 20:19:24 +0200

    Add QuotesService interface

--- src/quotes/quotes.service.ts
+++ src/quotes/quotes.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotImplementedException } from "@nestjs/common";
+import { QuoteInput } from "./models/quote-input.dto";
+import { QuoteMutation } from "./models/quote-mutation.dto";
+import { Quote } from "./models/quote-query.dto";
 
 @Injectable()
-export class QuotesService {}
+export class QuotesService {
+  private quotes: Quote[] = [
+    {
+      id: 0,
+      instrument: "AAPL",
+      timestamp: new Date(1621620906000),
+      price: 12600,
+    },
+    {
+      id: 1,
+      instrument: "GOOGL",
+      timestamp: new Date(1621620906 - 1000 * 3600 * 24),
+      price: 229538,
+    },
+    {
+      id: 3,
+      instrument: "AAPL",
+      timestamp: new Date(1621620906 - 1000 * 3600 * 24),
+      price: 12720,
+    },
+  ];
+
+  getall(): Quote[] {
+    throw new NotImplementedException();
+  }
+
+  getOne(quoteInput: QuoteInput): Quote {
+    throw new NotImplementedException();
+  }
+
+  addNew(quote: QuoteMutation): Quote {
+    throw new NotImplementedException();
+  }
+}
