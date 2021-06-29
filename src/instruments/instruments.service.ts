ab0f47b64d1c265ccbb9fb26e53a12266c807571
JacekCzupyt <kuba.czupyt@gmail.com>
2021-06-29 17:26:35 +0200

    Refactor addNew Instrument to use a transaction

--- src/instruments/instruments.service.ts
+++ src/instruments/instruments.service.ts
@@ -4,7 +4,12 @@ import {
   NotFoundException,
 } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { EntityNotFoundError, QueryFailedError, Repository } from "typeorm";
+import {
+  EntityNotFoundError,
+  getManager,
+  QueryFailedError,
+  Repository,
+} from "typeorm";
 import { InstrumentInput } from "./models/instrument-input.dto";
 import { Instrument } from "./models/instrument.entity";
 
@@ -41,16 +46,17 @@ export class InstrumentsService {
 
   async addNew(instrumentInput: InstrumentInput): Promise<Instrument> {
     try {
-      //transactions are enabled by default for the insert operation, therfore a duplicate instrument
-      //will never be inserted because of concurent requests, instead an QueryFailedError will be thrown
-      let result = await this.instrumentsRepository.insert({
-        ...instrumentInput,
-        instrumentName:
-          instrumentInput.instrumentName ?? instrumentInput.instrumentTicker,
+      return await getManager().transaction(async (manager) => {
+        await manager.insert(Instrument, {
+          ...instrumentInput,
+          instrumentName:
+            instrumentInput.instrumentName ?? instrumentInput.instrumentTicker,
+        });
+        return await manager.findOne(
+          Instrument,
+          instrumentInput.instrumentTicker
+        );
       });
-      return await this.instrumentsRepository.findOne(
-        instrumentInput.instrumentTicker
-      );
     } catch (e) {
       if (e instanceof QueryFailedError) {
         throw new BadRequestException(
